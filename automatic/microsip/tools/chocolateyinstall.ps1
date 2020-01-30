7cc1809a7b3480b03f656119138f7f550677ea72
Jan Müller <muj@dn.cz>
2020-01-30 11:26:28 +0100

    updates prior to automatic run for _1126

--- automatic/microsip/tools/chocolateyinstall.ps1
+++ automatic/microsip/tools/chocolateyinstall.ps1
@@ -1,16 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://www.microsip.org/downloads/?file=MicroSIP-3.19.25.exe'
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
+  url           = '{{DownloadUrl}}'
   url64bit      = $url64
   softwareName  = 'microsip*'
-  checksum      = 'f9a1b648f8186e77ee5ac0d75d25c4028a053b7f7195563ddb5c3e852f4baadb'
+  checksum      = '{{Checksum}}'
   checksumType  = 'sha256'
   silentArgs   = '/S'
   validExitCodes= @(0)
